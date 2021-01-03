253ac29081dca7d320b0174876775edf8dd0d28e
Mayukh <mayukh.gautam@gmail.com>
2021-01-03 18:08:31 -0500

    Rebase

--- Js/Square.js
+++ Js/Square.js
@@ -13,33 +13,6 @@ spot.prototype.draw = function(h, c){
   fill(c)
   rect(this.x*h, this.y*h, h, h)
 }
-//array.shift() REMOVES FIRST ITEM IN ARRAY
-spot.prototype.advance = function(p){
-  if(this.occ.length > 0){ //Capture
-    this.occ[0].alive = false
-    this.occ.shift()
-  }
-  append(this.occ, p)
-}
-//CLEAR OLD OCC
-spot.prototype.clear = function(){
-  this.occ.shift()
-}
-=======
-class spot{
-
-  constructor(x,y){
-    this.x = x
-    this.y = y
-    this.occ = []
-  }
-
-}
-
-spot.prototype.draw = function(h, c){
-  fill(c)
-  rect(this.x*h, this.y*h, h, h)
-}
 spot.prototype.getColor = function(){
   /* Used to flip the boards color while drawing */
   if(this.x % 2 == 0){
